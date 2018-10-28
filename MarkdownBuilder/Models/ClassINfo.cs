d4653d716d57b118cfafbcf0edf16aa0944f44bb
Adam O'Neil <adamosoftware@gmail.com>
2018-10-28 09:44:48 -0400

    added ClassInfo.Location

--- MarkdownBuilder/Models/ClassINfo.cs
+++ MarkdownBuilder/Models/ClassINfo.cs
@@ -7,7 +7,7 @@ namespace RoslynDocumentor.Models
 		public string Name { get; set; }
 		public bool IsStatic { get; set; }
 		public string Description { get; set; }
-		public string SourceFile { get; set; }
+		public Location Location { get; set; }
 		public IEnumerable<MethodInfo> Methods { get; set; }
 		public IEnumerable<PropertyInfo> Properties { get; set; }
 	}
