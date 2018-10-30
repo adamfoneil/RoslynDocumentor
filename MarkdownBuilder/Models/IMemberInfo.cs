b4879a65dea1d8ef71b10825e92cd82c33c917e2
Adam O'Neil <adamosoftware@gmail.com>
2018-10-30 16:35:21 -0400

    added comment to IMemberInfo.TypeName

--- MarkdownBuilder/Models/IMemberInfo.cs
+++ MarkdownBuilder/Models/IMemberInfo.cs
@@ -3,6 +3,9 @@
 	public interface IMemberInfo
 	{
 		string Name { get; }
+		/// <summary>
+		/// Name of a method or property's return type (BCL types should not have namespace, but solution-defined types should have namespace)
+		/// </summary>
 		string TypeName { get; set; }
 		/// <summary>
 		/// If the TypeName is defined in this solution, this is its location (otherwise null)
